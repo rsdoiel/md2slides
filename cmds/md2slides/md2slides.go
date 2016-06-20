2b5a43980e88f242d3c639341526a791d4ff4b8e
R. S. Doiel <rsdoiel@gmail.com>
2016-06-20 07:27:00 -0700

    fixed broken home link

--- cmds/md2slides/md2slides.go
+++ cmds/md2slides/md2slides.go
@@ -71,7 +71,7 @@ func makeSlide(tmpl *template.Template, slide *Slide) {
 
 func nextPrev(i, last int, fname string) string {
 	var p []string
-	p = append(p, fmt.Sprintf(`<a href="00-%s">home</a>`, fname))
+	p = append(p, fmt.Sprintf(`<a href="00-%s.html">home</a>`, fname))
 	if i > 0 {
 		// Include Previous
 		p = append(p,
