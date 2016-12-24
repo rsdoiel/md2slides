f7dd68ed00e774ba324ec837cf0e5645ae568ada
R. S. Doiel <rsdoiel@gmail.com>
2016-12-24 11:21:05 -0800

    Quick Save

--- mk-release.bash
+++ mk-release.bash
@@ -1,17 +1,28 @@
 #!/bin/bash
 #
-# Make releases for Linux/amd64, Linux/ARM6 and Linux/ARM7 (Raspberry Pi), Windows, and Mac OX X (darwin)
+# Make releases for Linux/amd64, Linux/ARM6 and Linux/ARM7 (Raspberry Pi), 
+# Windows, and Mac OX X (darwin)
 #
-RELEASE_NAME=mkslides
+PROJECT=mkslides
 
 PROG_LIST=mkslides
 
+VERSION=$(grep 'Version =' $PROJECT.go | cut -d\" -f 2)
+
+RELEASE_NAME=$PROJECT-$VERSION
+
+echo "Preparing $RELEASE_NAME"
 for PROGNAME in $PROG_LIST; do
+    echo "Cross compiling $PROGNAME"
   env GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
   env GOOS=darwin GOARCH=amd64 go build -o dist/macosx-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
   env GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-arm6/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
   env GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
   env GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
 done
-
-zip -r "$RELEASE_NAME-release.zip" README.md INSTALL.md LICENSE dist/*
+echo "Assembling dist"
+cp -v README.md dist/
+cp -v LICENSE dist/
+cp -v INSTALL.md dist/
+echo "Zipping up $RELEASE_NAME"
+zip -r "$RELEASE_NAME-release.zip" dist/*
